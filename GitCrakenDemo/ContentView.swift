170a7f822dc2ccbd25f8571a1ed9fff9855205eb
Pavlo Moroz <pavlo@sidekickinteractive.com>
2024-06-04 15:40:43 -0400

    Initial Commit

---
+++ GitCrakenDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitCrakenDemo
+//
+//  Created by Pavlo Moroz on 2024-06-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitCrakenDemo/GitCrakenDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitCrakenDemoApp.swift
+//  GitCrakenDemo
+//
+//  Created by Pavlo Moroz on 2024-06-04.
+//
+
+import SwiftUI
+
+@main
+struct GitCrakenDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
